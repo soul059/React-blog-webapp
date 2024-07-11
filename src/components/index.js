edd2c9134c61eecdc745c4d91ca8a54877e3f209
soul059 <keval059@outlook.com>
2024-07-11 10:14:36 +0530

    import bug fix in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Select from "./Select";
 import Header from "./header/header";
-import Footer from "./footer/Footer";
+import Footer from "./footer/footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
