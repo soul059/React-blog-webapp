8791adaf393213e544f00bc91006501d62c9adca
soul059 <keval059@outlook.com>
2024-07-14 17:05:22 +0530

    nothig prity much

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
                 <Container>
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full">
-                            <h1 className="text-2xl font-bold hover:text-white">
+                            <h1 className="text-2xl font-bold text-white">
                                 Login to read posts
                             </h1>
                         </div>
